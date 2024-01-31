Route3/blockscout

Define chain type

config/runtime/dev.exs
@@ -74,6 +74,13 @@ config :explorer, Explorer.Repo.Account,
   url: ExplorerConfigHelper.get_account_db_url(),
   pool_size: ConfigHelper.parse_integer_env_var("ACCOUNT_POOL_SIZE", 10)
 
+# Configure PolygonZkevm database
+config :explorer, Explorer.Repo.PolygonZkevm,
+  database: database,
+  hostname: hostname,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: ConfigHelper.parse_integer_env_var("POLYGON_ZKEVM_POOL_SIZE", 10)
+
 variant = Variant.get()
 
 Code.require_file("#{variant}.exs", "apps/explorer/config/dev")

config/runtime/prod.exs
@@ -53,6 +53,12 @@ config :explorer, Explorer.Repo.Account,
   pool_size: ConfigHelper.parse_integer_env_var("ACCOUNT_POOL_SIZE", 50),
   ssl: ExplorerConfigHelper.ssl_enabled?()
 
+# Configures PolygonZkevm database
+config :explorer, Explorer.Repo.PolygonZkevm,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: ConfigHelper.parse_integer_env_var("POLYGON_ZKEVM_POOL_SIZE", 50),
+  ssl: ExplorerConfigHelper.ssl_enabled?()
+
 variant = Variant.get()
 
 Code.require_file("#{variant}.exs", "apps/explorer/config/prod")

Apetree333/Blokscout

config/runtime/dev.exs
@@ -101,18 +101,18 @@ config :explorer, Explorer.Repo.RSK,
   # separating repos for different CHAIN_TYPE is implemented only for the sake of keeping DB schema update relevant to the current chain type
   pool_size: 1
 
-# Configure Suave database
-config :explorer, Explorer.Repo.Suave,
-  database: database,
-  hostname: hostname,
-  url: ExplorerConfigHelper.get_suave_db_url(),
-  pool_size: 1
-
 # Configure Shibarium database
 config :explorer, Explorer.Repo.Shibarium,
   database: database,
   hostname: hostname,
   url: System.get_env("DATABASE_URL"),
+  pool_size: 1
+
+# Configure Suave database
+config :explorer, Explorer.Repo.Suave,
+  database: database,
+  hostname: hostname,
+  url: ExplorerConfigHelper.get_suave_db_url(),
   pool_size: 1
 
 # Configures BridgedTokens database

config/runtime/prod.exs
@@ -51,6 +51,14 @@ config :explorer, Explorer.Repo.Account,
   ssl: ExplorerConfigHelper.ssl_enabled?(),
   queue_target: queue_target
 
+# Configures BridgedTokens database
+config :explorer, Explorer.Repo.BridgedTokens,
+  url: System.get_env("DATABASE_URL"),
+  # actually this repo is not started, and its pool size remains unused.
+  # separating repos for different CHAIN_TYPE is implemented only for the sake of keeping DB schema update relevant to the current chain type
+  pool_size: 1,
+  ssl: ExplorerConfigHelper.ssl_enabled?()
+
 # Configures PolygonEdge database
 config :explorer, Explorer.Repo.PolygonEdge,
   url: System.get_env("DATABASE_URL"),
@@ -75,23 +83,15 @@ config :explorer, Explorer.Repo.RSK,
   pool_size: 1,
   ssl: ExplorerConfigHelper.ssl_enabled?()
 
-# Configures Suave database
-config :explorer, Explorer.Repo.Suave,
-  url: ExplorerConfigHelper.get_suave_db_url(),
-  pool_size: 1,
-  ssl: ExplorerConfigHelper.ssl_enabled?()
-
 # Configures Shibarium database
 config :explorer, Explorer.Repo.Shibarium,
   url: System.get_env("DATABASE_URL"),
   pool_size: 1,
   ssl: ExplorerConfigHelper.ssl_enabled?()
 
-# Configures BridgedTokens database
-config :explorer, Explorer.Repo.BridgedTokens,
-  url: System.get_env("DATABASE_URL"),
-  # actually this repo is not started, and its pool size remains unused.
-  # separating repos for different CHAIN_TYPE is implemented only for the sake of keeping DB schema update relevant to the current chain type
+# Configures Suave database
+config :explorer, Explorer.Repo.Suave,
+  url: ExplorerConfigHelper.get_suave_db_url(),
   pool_size: 1,
   ssl: ExplorerConfigHelper.ssl_enabled?()
 